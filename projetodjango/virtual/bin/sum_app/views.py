aa24e2359d6b67f7032e84914febf2f926913d35
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-03-03 18:20:04 +0000

    django

--- projetodjango/virtual/bin/sum_app/views.py
+++ projetodjango/virtual/bin/sum_app/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-
-from django.shortcuts import render
-
+@csrf_exempt
 def sum_numbers(request):
-    num1 = request.GET.get('num1')
-    num2 = request.GET.get('num2')
+    num1 = request.POST.get('num1')
+    num2 = request.POST.get('num2')
+    print(f'num1: {num1}, num2: {num2}')  # Adicione esta linha para depuração
     result = None
     
-    if num1 and num2:
+    if num1 is not None and num2 is not None:
         result = int(num1) + int(num2)
     
-    return render(request, 'result.html', {'num1': num1, 'num2': num2, 'result': result})
-
+    return HttpResponse(f'A soma de {num1} e {num2} é {result}.')
